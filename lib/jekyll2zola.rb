574f3b720f94e5dba48cf673b090fc224295d85c
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2024-10-31 08:58:55 +0900

    create gem

---
+++ lib/jekyll2zola.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "jekyll2zola/version"
+
+module Jekyll2zola
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jekyll2zola/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Jekyll2zola
+  VERSION = "0.1.0"
+end
